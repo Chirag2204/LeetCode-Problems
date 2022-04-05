8a5e126d6631e05bd40d649cd7e7d9ebb5c47617
Chirag2204 <72817113+Chirag2204@users.noreply.github.com>
2022-04-05 10:52:15 +0530

    Time: 36 ms (90.16%), Space: 19.1 MB (66.73%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(),intervals.end(),sortcol);
+      vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(int i=1;i<intervals.size();i++){
+            if(ans.back()[1] >= intervals[i][0]) 
+                ans.back()[1] = max(ans.back()[1] , intervals[i][1]);
+            else ans.push_back(intervals[i]);
+        }
+        return ans;
+    }
+    
+    static bool sortcol(const vector<int>& v1, const vector<int>& v2)
+    {
+        return v1[0] < v2[0];
+    }
+    
+};
