692716099dc097ad31c3ebc27c2d3aa920e4daa8
Chirag2204 <72817113+Chirag2204@users.noreply.github.com>
2022-04-08 09:11:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (37.13%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.cpp
@@ -4,7 +4,7 @@ public:
         
          int count=0;
         int mid=n%2==0? n : 2*(n/2)+1;
-        for(int i=0;i<n;i++){
+        for(int i=n/2;i<n;i++){
             count+=max(0,(2*i + 1)-mid);
         }
         return count;
