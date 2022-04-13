e0e373b88fe289589e82ed82d9af10015959533d
Chirag2204 <72817113+Chirag2204@users.noreply.github.com>
2022-04-13 23:41:04 +0530

    Time: 52 ms (89.92%), Space: 25.8 MB (67.19%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-    bool isSubArray(vector<int> nums,int start,int end ){
-        sort(nums.begin()+start,nums.begin()+end+1);
-        int dif=nums[start+1]-nums[start];
-        
-        for(int i=start;i<=end-1;i++){
+    bool isSubArray(vector<int>& nums){
+        sort(nums.begin(),nums.end());
+        int dif=nums[1]-nums[0];
+       
+        for(int i=0;i<nums.size()-1;i++){
             if(nums[i+1]-nums[i]!=dif)
                 return false;
         }
@@ -13,7 +13,8 @@ public:
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
         vector<bool> ans;
         for(int i=0;i<l.size();i++){
-            ans.push_back(isSubArray(nums,l[i],r[i]));
+            vector<int> topass(nums.begin()+l[i],nums.begin()+r[i]+1);
+            ans.push_back(isSubArray(topass));
         }
         return ans;
     }
