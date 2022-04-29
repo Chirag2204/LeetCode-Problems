2a2212766554b0d4c7ea969b60e449fae00e0004
Chirag2204 <72817113+Chirag2204@users.noreply.github.com>
2022-04-29 22:07:07 +0530

    Time: 147 ms (29.15%), Space: 30.3 MB (33.78%) - LeetHub

---
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        priority_queue<int> pq;
+        for(int i:nums){
+            pq.push(i);
+        }
+        int i=nums.size()-1;
+        while(!pq.empty()){
+            nums[i]=pq.top();
+            pq.pop();
+            i--;
+        }
+        return nums;
+    }
+};
