0600e53d9a53d4e98bc71838177d560746014958
Chirag2204 <72817113+Chirag2204@users.noreply.github.com>
2022-05-01 19:01:56 +0530

    Time: 7 ms (6.96%), Space: 6.4 MB (39.15%) - LeetHub

---
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+       stack<char> s1;
+        stack<char> t1;
+        
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='#'){
+                if(!s1.empty()){
+                    s1.pop();
+                }
+            }else{
+                s1.push(s[i]);
+            }
+        }
+        
+        for(int i=0;i<t.length();i++){
+            cout<<t[i]<<"->";
+            if(t[i]=='#'){
+                if(!t1.empty())t1.pop();
+            }else{
+                t1.push(t[i]);
+            }
+        }
+        s.clear();
+        t.clear();
+        while(!s1.empty()){
+            s.push_back(s1.top());
+            s1.pop();
+        }
+        
+        while(!t1.empty()){
+            t.push_back(t1.top());
+            t1.pop();
+        }
+        cout<<s<<" "<<t<<endl;
+        return s==t;
+    }
+};
