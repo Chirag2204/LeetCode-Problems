08f0492f9dfa61e4df748de56689cc9f97284df5
Chirag2204 <72817113+Chirag2204@users.noreply.github.com>
2022-07-08 12:41:42 +0530

    Time: 91 ms (22.20%), Space: 21.9 MB (42.96%) - LeetHub

---
+++ 817-linked-list-components/817-linked-list-components.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        map<int,int> m;
+        for(int& i:nums){
+            m[i]=1;
+        }
+        int ans=nums.size();
+        while(head->next!=NULL){
+            if(m.count(head->val) &&  m.count(head->next->val))ans--;
+            head=head->next;
+        }
+        return ans;
+        
+    }
+};
