e067aaf9355c60349d77cba5e87e3ea96f12c9d4
Chirag Chandak <72817113+Chirag2204@users.noreply.github.com>
2022-08-09 17:02:12 +0530

    Time: 71 ms (8.34%), Space: 17.3 MB (5.08%) - LeetHub

---
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    void dfs(map<int,unordered_set<int>> &m,vector<int>& visited,int val){
+        if(visited[val])return ;
+        visited[val]=1;
+        for(auto& x:m[val]){
+            dfs(m,visited,x);
+        }
+    }
+    
+    int findCircleNum(vector<vector<int>>& nums) {
+      map<int,unordered_set<int>> m;
+        int n=nums.size();
+      for(int i=0;i<n;i++){
+          for(int j=0;j<n;j++){
+              if(nums[i][j]){
+                 m[i].insert(j);
+                  m[j].insert(i);
+              }
+          }
+      }
+        int ans=0;
+        vector<int> visited(n+1,0);
+        for(auto& s:m){
+           if(visited[s.first]==0) {
+               ans++;
+               int val=s.first;
+               dfs(m,visited,val);
+           }
+        }
+        return ans;
+    }
+};
