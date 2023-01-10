44909b8e9d7f5291516a792c5acabb13db7b981d
Chirag Chandak <72817113+Chirag2204@users.noreply.github.com>
2023-01-11 00:33:35 +0530

    Added solution - LeetHub

---
+++ Make array elements unique - GFG/make-array-elements-unique.cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    long long int minIncrements(vector<int> arr, int N) {
+        long long ans=0;
+        unordered_set<int> s;
+        long long prev=0;
+        
+        sort(arr.begin(),arr.end());
+        
+        for(int &i:arr){
+            if(!s.count(i)){
+                s.insert(i);
+                prev=i;
+            }else{
+                ans+= abs(i-prev)+1;
+                prev++;
+                s.insert(prev);
+            }
+        }
+        return ans;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        cout << obj.minIncrements(arr, N) << endl;
+    }
+}
+// } Driver Code Ends
